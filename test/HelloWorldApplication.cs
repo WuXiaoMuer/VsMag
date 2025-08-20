98f881827d527086a238ab2b51f6626759df6b7a
WuXiaoMuer <wu20969@qq.com>
2025-08-20 16:43:02 +0800

    update -2022.5.2

---
+++ test/HelloWorldApplication.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HelloWorldApplication
+{
+	/* 类名 HelloWorld */
+	class HelloWorld
+	{
+		/* main主函数 */
+		static void Main(string[] args)
+		{
+			/* 打印和暂停函数 */
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
